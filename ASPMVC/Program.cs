4cd813a1b482649747746f02458b6c77430cc135
Vladisav Vasiljev <vladmaster29@gmail.com>
2023-02-16 07:57:11 +0300

    2 commit

--- ASPMVC/Program.cs
+++ ASPMVC/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Departments}/{action=Index}/{id?}");
 
 app.Run();
